b28975dd8546d34878a37f4d7321b1c933c7117a
Jaejuna <anayana9988@hanyang.ac.kr>
2022-09-25 11:52:47 +0000

    ch01 done

--- Ch1/scrapetest.py
+++ Ch1/scrapetest.py
@@ -2,19 +2,27 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
+#
 def getTitle(url):
     try:
+        # url 함수 임포트 -> html 변수에 넣기
         html = urlopen(url)
+        #웹 예외 처리
     except HTTPError as e:
         return None
     try:
+        #read() 함수 호출하지 않고 urlopen이 반환하는 파일 객체를 바로 사용
+        #첫 번째 매개변수는 HTML 텍스트
+        #두 번째 매개변수는 구문 분석기이다.
         bs = BeautifulSoup(html.read(), 'html.parser')
+        # 타이틀인 body태그 안에 h1 태크 속성 변수에 넣기
         title = bs.body.h1
     except AttributeError as e:
         return None
     return title
 
 title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+
 if title == None:
     print('Title could not be found')
 else:
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Web_scraping_with_python
+
+ref: http://www.yes24.com/Product/Goods/71047040
